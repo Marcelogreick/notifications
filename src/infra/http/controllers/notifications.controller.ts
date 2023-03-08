e276a4a60a54f806b1959ff4e40ced674a54a311
Marcelogreick <mgreick25@gmail.com>
2023-03-07 22:37:00 -0300

    criando mapper de notificacao

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,28 @@
+import { SendNotification } from '@application/use-cases/send.notification';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 
 @Controller('notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     const { content, category, recipientId } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return {
+      notification: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+      },
+    };
   }
 }
