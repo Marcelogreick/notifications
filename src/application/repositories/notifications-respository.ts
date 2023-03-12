4594bd15afdcd5b152d2b8a1129fb479e543c5f8
Marcelogreick <mgreick25@gmail.com>
2023-03-12 17:04:52 -0300

    alterando cancelar notificacao

--- src/application/repositories/notifications-respository.ts
+++ src/application/repositories/notifications-respository.ts
@@ -4,4 +4,6 @@ export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract findById(notificationd: string): Promise<Notification | null>;
   abstract save(notification: Notification): Promise<void>;
+  abstract countManyByRecipientId(recipientId: string): Promise<number>;
+  abstract findManyByRecipientId(recipientId: string): Promise<Notification[]>;
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -18,6 +18,12 @@ export class InMemoryNotificationsRepository
     );
   }
 
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (item) => item.recipientId === recipientId,
+    );
+  }
+
   async countManyByRecipientId(recipientId: string): Promise<number> {
     return this.notifications.filter((item) => item.recipientId === recipientId)
       .length;
