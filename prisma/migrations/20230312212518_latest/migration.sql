4504c2314e537233eab8284163b273b8ecc90c8d
Marcelogreick <mgreick25@gmail.com>
2023-03-12 18:29:20 -0300

    adicionando configs

--- prisma/migrations/20230312212518_latest/migration.sql
+++ prisma/migrations/20230312212518_latest/migration.sql
@@ -1,13 +1,14 @@
 -- CreateTable
-CREATE TABLE "notification" (
+CREATE TABLE "Notification" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "recipientId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "readAt" DATETIME,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "canceledAt" DATETIME,
     "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateIndex
-CREATE INDEX "notification_recipientId_idx" ON "notification"("recipientId");
+CREATE INDEX "Notification_recipientId_idx" ON "Notification"("recipientId");
