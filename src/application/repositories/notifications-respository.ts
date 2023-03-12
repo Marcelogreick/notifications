1b8b19c92fdc3efb710f0c30a73459aed63ac97a
Marcelogreick <mgreick25@gmail.com>
2023-03-12 13:24:09 -0300

    adicionando coluna canceled at

--- src/application/repositories/notifications-respository.ts
+++ src/application/repositories/notifications-respository.ts
@@ -2,4 +2,5 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationd: string): Promise<Notification | null>;
 }
